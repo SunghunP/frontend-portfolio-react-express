c3d7cec17dd4f1e7de1f6ea2c813ecfa229cdb04
Joe Park <sunghun.josephp@gmail.com>
2022-05-21 14:54:25 -0700

    Setup Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,14 @@
+import { Link } from "react-router-dom";
+
 export default function Header() {
   return (
-    <div>THIS IS THE HEADER COMPONENT</div>
+    <header>
+      <h1>Sunghun Park's Portfolio</h1>
+      <nav>
+        <Link to="/" >HOME</Link>
+        <Link to="/about" >ABOUT</Link>
+        <Link to="/projects" >PROJECTS</Link>
+      </nav>
+    </header>
   )
 }
