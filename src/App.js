ffa1886c9a74c81782242ad4635b9aa7a2e3f703
Joe Park <sunghun.josephp@gmail.com>
2022-05-21 14:51:48 -0700

    Setup Routes in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+// Import components
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import About from './pages/About';
 
 function App() {
+  const URL = "https://backend-portfolio-sunghunp.herokuapp.com/"
+
   return (
     <div className="App">
-
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects/" element={<Projects URL={URL}/> } />
+        <Route path="/about/" element={<About URL={URL} />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
