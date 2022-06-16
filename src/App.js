9d72310d28cf013083bef81aa778a0b3c4889ee6
Joe Park <sunghun.josephp@gmail.com>
2022-06-16 16:58:59 -0700

    Refactor styling into styled components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './styles.sass';
 // Import components
 import Header from './components/Header';
 import Footer from './components/Footer';
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,29 @@
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+const StyledHeader = styled.header`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 15px;
+  padding: 5px;
+`
 
+const StyledNav = styled.nav`
+  display: flex;
+  justify-content: space-around;
+  border: 3px solid black;
+  padding: 8px;
+  width: 90%;
+  margin: auto;
+`
 export default function Header() {
-  const navStyle = {
-    display: "flex",
-    justifyContent: "space-around",
-    border: "3px solid black",
-    padding: "8px",
-    width: "90%",
-    margin: "auto",
-  };
-
   return (
-    <header>
+    <StyledHeader>
       <h1>Sunghun Park's Portfolio</h1>
-      <nav style={navStyle}>
+      <StyledNav>
         <Link to="/" >HOME</Link>
         <Link to="/about" >ABOUT</Link>
         <Link to="/projects" >PROJECTS</Link>
-      </nav>
-    </header>
+      </StyledNav>
+    </StyledHeader>
   )
 }
