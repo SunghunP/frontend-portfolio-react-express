2d0e2c16686e5e315f4fe3c3c1b804a6202091b7
Joe Park <sunghun.josephp@gmail.com>
2022-06-16 19:18:28 -0700

    Add a function to display our welcome message like a type writer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,42 @@
+import styled from "styled-components";
+import { useEffect, useState } from "react";
+
+const StyledDiv = styled.div`
+  text-align: center;
+  margin: auto 0 ;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+
+const StyledH1 = styled.h1`
+  text-align: center;
+  overflow: hidden;
+`;
+
 export default function Home() {
+  const [count, setCount] = useState(0);
+  const [message, setMessage] = useState('');
+  const msg = "Welcome to Sunghun's Portfolio";
+  const msgArr = msg.split('');
+
+  function typeMessage() {
+    setMessage(message + msg[count])
+  };
+
+  useEffect(() => {
+    if (count < msg.length) {
+      typeMessage();
+      setTimeout(() => {
+        setCount(count + 1)
+      }, 100);
+    };
+  }, [count]);
+
   return (
-    <div>This is the home component</div>
-  )
-}
+    <StyledDiv className="home-page">
+      <StyledH1 id="msg">{message}</StyledH1>
+    </StyledDiv>
+  );
+};
