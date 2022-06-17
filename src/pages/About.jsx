fd723e5c24219a70a1f9a42162b1eeed0bb1d1ab
Joe Park <sunghun.josephp@gmail.com>
2022-06-16 19:29:56 -0700

    Add links to about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,14 @@
 import { useEffect, useState } from "react";
+import styled from "styled-components";
+
+const StyledDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 30px;
+  padding: 10px;
+`
 
 export default function About({ URL }) {
   // create state to hold data from backend
@@ -16,11 +26,16 @@ export default function About({ URL }) {
 
   function loaded() {
     return (
-      <div>
+      <StyledDiv className="about">
         <h2>{about.name}</h2>
         <h3>{about.email}</h3>
         <p>{about.bio}</p>
-      </div>
+        <h2>Find me on these Websites!</h2>
+        <ul>
+          <li><h3><a href="https://www.linkedin.com/in/sunghunp/">Linkedin</a></h3></li>
+          <li><h3><a href="https://github.com/SunghunP">Github</a></h3></li>
+        </ul>
+      </StyledDiv>
     );
   };
 
