4fe3e4a18bcfcda06c25e9170823fe325ab67104
Joe Park <sunghun.josephp@gmail.com>
2022-06-16 17:21:25 -0700

    Style the footer to make sure it stays on the bottom of the page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,23 @@
+import styled from 'styled-components';
+
+const StyledFooter = styled.footer`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: auto;
+  border-top: 2px solid black;
+  p {
+    margin: 10px;
+    padding: 10px; 
+  }
+`
+
 export default function Footer() {
   return (
-    <div>
-      Hello this is the footer thingy
-    </div>
-  )
-}
+    <StyledFooter>
+      <p><a href="https://www.linkedin.com/in/sunghunp/">Linkedin</a></p>
+      <p><a href="https://github.com/SunghunP">Github</a></p>
+      
+    </StyledFooter>
+  );
+};
